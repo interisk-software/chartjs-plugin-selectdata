b8cd845f04528fde9b45c1446b65b7f9a75cd34a
luancaike <luan.caike@acad.ifma.edu.br>
2021-06-07 11:50:06 -0300

    feat: added functionality clearSelection and selectDataIndex

--- src/plugin/chartjs-plugin-selectdata.js
+++ src/plugin/chartjs-plugin-selectdata.js
@@ -12,106 +12,132 @@ globals.plugins.selectdata = {
   onSelectionClear: undefined
 };
 
-const SelectionDataPlugin = {
-  id: 'selectdata',
-  afterEvent(chart, chartEvent, options) {
-    const eventType = chartEvent.type || chartEvent.event.type;
-    if (eventType === 'click') {
-      let selected = [];
+const selectIndexDataSet = function(chart, selectedIndex) {
+  let clearSelection = false;
+  const ifNeedClearSelection = (dataset, resetKeys = []) => {
+    if (dataset[EXPANDO_INDEX] === selectedIndex) {
+      clearSelection = true;
+      dataset[EXPANDO_INDEX] = null;
+      resetKeys.forEach(key => {
+        dataset[key] = dataset[EXPANDO_COLOR][key];
+      });
+      return true;
+    }
 
-      if (chart.getElementsAtEvent) {
-        selected = chart.getElementsAtEvent(chartEvent);
-      } else {
-        selected = chart.getElementsAtEventForMode(chartEvent.event.native, 'index', {intersect: true}, false);
+    dataset[EXPANDO_INDEX] = selectedIndex;
+    return false;
+  };
+
+  const checkExpandoKeys = (dataset, checkKeys = []) => {
+    checkKeys.forEach(key => {
+      if (dataset[EXPANDO_COLOR] && dataset[EXPANDO_COLOR][key] !== undefined) {
+        dataset[key] = dataset[EXPANDO_COLOR][key];
       }
+    });
+  };
 
-      if (selected && !selected.length) {
+  const setColorsAlphaNotSelected = (dataset, key) => {
+    if (Array.isArray(dataset[key]) && dataset[key].length) {
+      dataset[EXPANDO_COLOR][key] = dataset[key];
+      dataset[key] = dataset[key]
+        .map((color, colorIndex) => (selectedIndex !== colorIndex ? helpers.color(color).alpha(alpha).rgbString() : color));
+    }
+    if (typeof dataset[key] === 'string') {
+      dataset[EXPANDO_COLOR][key] = dataset[key];
+      dataset[key] = dataset.data
+        .map((e, colorIndex) => (selectedIndex !== colorIndex ? helpers.color(dataset[key]).alpha(alpha).rgbString() : dataset.pointBackgroundColor));
+    }
+  };
+  const setColorsAlphaAll = (dataset, key) => {
+    if (typeof dataset[key] === 'string') {
+      dataset[EXPANDO_COLOR][key] = dataset[key];
+      dataset[key] = helpers.color(dataset[key]).alpha(alpha).rgbString();
+    }
+    if (Array.isArray(dataset[key]) && dataset[key].length) {
+      dataset[EXPANDO_COLOR][key] = dataset[key];
+      dataset[key] = dataset[key].map((color) => helpers.color(color).alpha(alpha).rgbString());
+    }
+  };
+
+  chart.config.data.datasets.forEach((dataset) => {
+    if (!dataset[EXPANDO_COLOR]) {
+      dataset[EXPANDO_COLOR] = {};
+    }
+    switch (dataset.type || chart.config.type) {
+    case 'line':
+    case 'radar':
+    case 'scatter':
+      dataset.pointBackgroundColor = dataset.pointBackgroundColor === undefined ? dataset.borderColor || 'rgb(0,0,0)' : dataset.pointBackgroundColor;
+      if (ifNeedClearSelection(dataset, ['pointBackgroundColor', 'borderColor'])) {
         return;
       }
-      const selectedIndex = selected[0]._index || selected[0].index;
-      let clearSelection = false;
+      checkExpandoKeys(dataset, ['pointBackgroundColor', 'borderColor']);
+      setColorsAlphaNotSelected(dataset, 'pointBackgroundColor');
+      setColorsAlphaAll(dataset, 'borderColor');
+      break;
+    default:
+      if (ifNeedClearSelection(dataset, ['backgroundColor'])) {
+        return;
+      }
+      checkExpandoKeys(dataset, ['backgroundColor']);
+      setColorsAlphaNotSelected(dataset, 'backgroundColor');
+      break;
+    }
+  });
 
-      const ifNeedClearSelection = (dataset, resetKeys = []) => {
-        if (dataset[EXPANDO_INDEX] === selectedIndex) {
-          clearSelection = true;
-          dataset[EXPANDO_INDEX] = null;
-          resetKeys.forEach(key => {
-            dataset[key] = dataset[EXPANDO_COLOR][key];
-          });
-          return true;
-        }
+  chart.update();
+  return clearSelection;
+};
 
-        dataset[EXPANDO_INDEX] = selectedIndex;
-        return false;
-      };
+const emitEventSelection = function(selectedIndex, selected, clearSelection, options) {
+  const params = {
+    datasetIndex: selected.map((el) => el._datasetIndex || el.datasetIndex),
+    index: selectedIndex,
+  };
+  if (clearSelection && options.onSelectionClear) {
+    options.onSelectionClear(params);
+  } else if (options.onSelection) {
+    options.onSelection(params);
+  }
+};
 
-      const checkExpandoKeys = (dataset, checkKeys = []) => {
-        checkKeys.forEach(key => {
-          if (dataset[EXPANDO_COLOR] && dataset[EXPANDO_COLOR][key]) {
+const SelectionDataPlugin = {
+  id: 'selectdata',
+  afterInit(chart) {
+    chart.clearSelection = function() {
+      chart.config.data.datasets.forEach((dataset) => {
+        dataset[EXPANDO_INDEX] = null;
+        Object.keys(dataset).forEach(key => {
+          if (dataset[EXPANDO_COLOR] && dataset[EXPANDO_COLOR][key] !== undefined) {
             dataset[key] = dataset[EXPANDO_COLOR][key];
           }
         });
-      };
-
-      const setColorsAlphaNotSelected = (dataset, key) => {
-        if (Array.isArray(dataset[key]) && dataset[key].length) {
-          dataset[EXPANDO_COLOR][key] = dataset[key];
-          dataset[key] = dataset[key]
-            .map((color, colorIndex) => (selectedIndex !== colorIndex ? helpers.color(color).alpha(alpha).rgbString() : color));
-        }
-        if (typeof dataset[key] === 'string') {
-          dataset[EXPANDO_COLOR][key] = dataset[key];
-          dataset[key] = dataset.data
-            .map((e, colorIndex) => (selectedIndex !== colorIndex ? helpers.color(dataset[key]).alpha(alpha).rgbString() : dataset.pointBackgroundColor));
-        }
-      };
-      const setColorsAlphaAll = (dataset, key) => {
-        if (typeof dataset[key] === 'string') {
-          dataset[EXPANDO_COLOR][key] = dataset[key];
-          dataset[key] = helpers.color(dataset[key]).alpha(alpha).rgbString();
-        }
-        if (Array.isArray(dataset[key]) && dataset[key].length) {
-          dataset[EXPANDO_COLOR][key] = dataset[key];
-          dataset[key] = dataset[key].map((color) => helpers.color(color).alpha(alpha).rgbString());
-        }
-      };
-
-      chart.config.data.datasets.forEach((dataset) => {
-        if (!dataset[EXPANDO_COLOR]) {
-          dataset[EXPANDO_COLOR] = {};
-        }
-        switch (dataset.type || chart.config.type) {
-        case 'line':
-        case 'radar':
-        case 'scatter':
-          dataset.pointBackgroundColor = dataset.pointBackgroundColor === undefined ? dataset.borderColor || 'rgb(0,0,0)' : dataset.pointBackgroundColor;
-          if (ifNeedClearSelection(dataset, ['pointBackgroundColor', 'borderColor'])) {
-            return;
-          }
-          checkExpandoKeys(dataset, ['pointBackgroundColor', 'borderColor']);
-          setColorsAlphaNotSelected(dataset, 'pointBackgroundColor');
-          setColorsAlphaAll(dataset, 'borderColor');
-          break;
-        default:
-          if (ifNeedClearSelection(dataset, ['backgroundColor'])) {
-            return;
-          }
-          checkExpandoKeys(dataset, ['backgroundColor']);
-          setColorsAlphaNotSelected(dataset, 'backgroundColor');
-          break;
-        }
       });
-
       chart.update();
-      const params = {
-        datasetIndex: selected.map((el) => el._datasetIndex || el.datasetIndex),
-        index: selectedIndex,
-      };
-      if (clearSelection && options.onSelectionClear) {
-        options.onSelectionClear(params);
-      } else if (options.onSelection) {
-        options.onSelection(params);
+    };
+    chart.selectDataIndex = function(index) {
+      selectIndexDataSet(chart, index);
+      chart.update();
+    };
+  },
+  afterEvent(chart, chartEvent, options) {
+    const eventType = chartEvent.type || chartEvent.event.type;
+    if (eventType === 'click') {
+
+      let selectedElements = [];
+
+      if (chart.getElementsAtEvent) {
+        selectedElements = chart.getElementsAtEvent(chartEvent);
+      } else {
+        selectedElements = chart.getElementsAtEventForMode(chartEvent.event.native, 'index', {intersect: true}, false);
       }
+
+      if (selectedElements && !selectedElements.length) {
+        return;
+      }
+      const selectedIndex = selectedElements[0]._index === undefined ? selectedElements[0].index : selectedElements[0]._index;
+      let clearSelection = selectIndexDataSet(chart, selectedIndex, options);
+      emitEventSelection(selectedIndex, selectedElements, clearSelection, options);
     }
   },
 };
